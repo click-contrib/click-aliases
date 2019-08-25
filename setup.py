ec6dae67dff25d97eb2ea6cb2c522850bb69a3cd
Robbin Bonthond <robbin.bonthond@methodics.com>
2019-08-24 21:11:17 -0500

    preparing PyPi release

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@
 
 from setuptools import setup
 
-with open('README.md') as f:
-    readme = f.read()
+with open('README.md', 'r') as f:
+    long_description = f.read()
 
 setup(
     name='click-aliases',
     version='1.0.0',
     description='Enable aliases for Click',
-    long_description=readme,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Robbin Bonthond',
     author_email='robbin@bonthond.com',
     url='https://github.com/click-contrib/click-aliases',
