66d38d1672da52b06796e77a2f435563ed720f61
Robbin Bonthond <robbin@bonthond.com>
2024-10-17 10:41:27 -0500

    updated for v1.0.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md") as f:
 
 setup(
     name="click-aliases",
-    version="1.0.4",
+    version="1.0.5",
     description="Enable aliases for Click",
     long_description=long_description,
     long_description_content_type="text/markdown",
